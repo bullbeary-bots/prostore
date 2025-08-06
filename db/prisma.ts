4dbc20256159e73c1690796a5625799ced01d6ef
bullbeary-bots <bullbeary.bots@outlook.com>
2025-08-06 14:36:56 +0200

    Added auth and sign in

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,31 +1,31 @@
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
 
-// import { PrismaClient } from "@/lib/generated/prisma";
-// import { Pool, neonConfig } from "@neondatabase/serverless";
-// import { PrismaNeon } from "@prisma/adapter-neon";
-// import ws from "ws";
+import { PrismaClient } from "@/lib/generated/prisma";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import ws from "ws";
 
-// dotenv.config();
-// neonConfig.webSocketConstructor = ws;
-// const connectionString = `${process.env.DATABASE_URL}`;
+dotenv.config();
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
 
-// const pool = new Pool({ connectionString });
-// const adapter = new PrismaNeon(pool);
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
 
-// export const prisma = new PrismaClient({ adapter }).$extends({
-//   result: {
-//     product: {
-//       price: {
-//         compute(product) {
-//           return product.price.toString();
-//         },
-//       },
-//       rating: {
-//         compute(product) {
-//           return product.rating.toString();
-//         },
-//       },
-//     },
-//   },
-// });
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
+});
